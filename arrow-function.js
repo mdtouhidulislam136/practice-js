49c30007c3b9eff549a22f7537c77ce7a50dfaa0
mdtouhidulislam136 <mdtouhidul.islam136@gmail.com>
2022-02-28 23:51:59 +0200

    two parameter arrow function sum and division

--- arrow-function.js
+++ arrow-function.js
@@ -1,10 +1,11 @@
 
-const zero = (number) => {
+const zero = (number1, number2) => {
 
-    const num = number/7;
-    return num;
+    const sum = number1+ number2;
+    const div = sum / 2;
+    return div;
 }
 
-const result = zero(49.5);
+const result = zero(49, 30);
 
 console.log(result.toFixed(2));
